c72e632f2f50355656ce18ce3bdd2bf2af21d8e6
WaleedAlrawi <waleedalrawi@hotmail.com>
2022-07-27 17:51:58 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./customStyles.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      initialValues={{
+        emailField: "",
+        pswField: "",
+      }}
+      onSubmit={(values) => {
+        alert("Login Successful");
+      }}
+      validationSchema={Yup.object({
+        emailField: Yup.string()
+          .max(18, "Must be 20 Characters or less")
+          .email("Username should be an email")
+          .required("Field Required"),
+        pswField: Yup.string()
+          .max(18, "Must be 20 Characters or less")
+          .required("Field Required"),
+      })}
+    >
+      <Form>
+        <label htmlFor="emailField"> Username: </label>
+        <Field
+          id="emailField"
+          name="emailField"
+          type="email"
+          placeHolder="john@gmail.com"
+        />
+        <br />
+        <ErrorMessage
+          name="emailField"
+          component="div"
+          id="emailError"
+          className="error"
+        />
+
+        <label htmlFor="pswField"> Password: </label>
+        <Field
+          id="pswField"
+          name="pswField"
+          type="text"
+          placeHolder="password"
+        />
+        <br />
+        <ErrorMessage
+          name="pswField"
+          component="div"
+          id="pswError"
+          className="error"
+        />
+
+        <br />
+        <button type="LOGIN"> SUBMIT </button>
+      </Form>
+    </Formik>
   );
 }
+//can also define custom resuseable input primitive components
+//see https://formik.org/docs/tutorial#leveraging-react-context
 
 export default App;
